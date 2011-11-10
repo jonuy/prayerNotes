417d53b4570d6b2efaaa04d8eb256184a28774df
jdroyd <jdroyd@gmail.com>
2011-11-10 14:07:15 -0500

    forgot to include in last check in

--- gen/com/jdroyd/prayerNotes/R.java
+++ gen/com/jdroyd/prayerNotes/R.java
@@ -11,11 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_menu_search_holodark=0x7f020000;
-        public static final int ic_new_note_holodark=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int img_gallery=0x7f020003;
-        public static final int img_remove_icon=0x7f020004;
+        public static final int ic_back_unselected=0x7f020000;
+        public static final int ic_delete_unselected=0x7f020001;
+        public static final int ic_menu_save=0x7f020002;
+        public static final int ic_menu_search_holodark=0x7f020003;
+        public static final int ic_new_note_holodark=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int img_gallery=0x7f020006;
+        public static final int img_remove_icon=0x7f020007;
     }
     public static final class id {
         public static final int alarm_main_date=0x7f070002;
@@ -24,30 +27,35 @@ public final class R {
         public static final int alarm_main_set_container=0x7f070003;
         public static final int alarm_main_time=0x7f070001;
         public static final int alarm_main_time_button=0x7f070005;
-        public static final int edit_note_alarm=0x7f07000f;
-        public static final int edit_note_alarm_remove=0x7f070011;
-        public static final int edit_note_alarm_status=0x7f070010;
-        public static final int edit_note_discard=0x7f07000e;
-        public static final int edit_note_img=0x7f070007;
-        public static final int edit_note_img_remove=0x7f070008;
-        public static final int edit_note_prayedFor=0x7f07000a;
-        public static final int edit_note_prayedFor_status=0x7f07000b;
+        public static final int edit_banner=0x7f070007;
+        public static final int edit_banner_separator_0=0x7f070009;
+        public static final int edit_banner_separator_1=0x7f07000b;
+        public static final int edit_banner_separator_2=0x7f07000d;
+        public static final int edit_note_alarm=0x7f070014;
+        public static final int edit_note_alarm_remove=0x7f070016;
+        public static final int edit_note_alarm_status=0x7f070015;
+        public static final int edit_note_cancel=0x7f070008;
+        public static final int edit_note_discard=0x7f07000a;
+        public static final int edit_note_img=0x7f07000e;
+        public static final int edit_note_img_remove=0x7f07000f;
+        public static final int edit_note_prayedFor=0x7f070011;
+        public static final int edit_note_prayedFor_status=0x7f070012;
         public static final int edit_note_save=0x7f07000c;
-        public static final int edit_note_share=0x7f07000d;
-        public static final int edit_note_text=0x7f070009;
+        public static final int edit_note_share=0x7f070013;
+        public static final int edit_note_text=0x7f070010;
         public static final int layout_alarm_main=0x7f070000;
-        public static final int main_admob_banner=0x7f070018;
-        public static final int main_banner=0x7f070012;
-        public static final int main_banner_separator_0=0x7f070014;
-        public static final int main_banner_separator_1=0x7f070016;
-        public static final int main_button_add=0x7f070013;
-        public static final int main_button_search=0x7f070015;
-        public static final int main_div_banner=0x7f070017;
-        public static final int main_note_row_created=0x7f07001c;
-        public static final int main_note_row_created_text=0x7f07001b;
-        public static final int main_note_row_img=0x7f070019;
-        public static final int main_note_row_status=0x7f07001d;
-        public static final int main_note_row_text=0x7f07001a;
+        public static final int main_admob_banner=0x7f07001d;
+        public static final int main_banner=0x7f070017;
+        public static final int main_banner_separator_0=0x7f070019;
+        public static final int main_banner_separator_1=0x7f07001b;
+        public static final int main_button_add=0x7f070018;
+        public static final int main_button_search=0x7f07001a;
+        public static final int main_div_banner=0x7f07001c;
+        public static final int main_note_row_created=0x7f070021;
+        public static final int main_note_row_created_text=0x7f070020;
+        public static final int main_note_row_img=0x7f07001e;
+        public static final int main_note_row_status=0x7f070022;
+        public static final int main_note_row_text=0x7f07001f;
     }
     public static final class layout {
         public static final int alarm_main=0x7f030000;
